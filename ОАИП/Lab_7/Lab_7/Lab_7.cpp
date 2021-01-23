abed7bb1d976387184ab2f3f0b8dc038ce18e03d
psshamshin <57655945+psshamshin@users.noreply.github.com>
2021-01-23 10:27:26 +0300

    add new labs
    Added lab #7 and #8

---
+++ ОАИП/Lab_7/Lab_7/Lab_7.cpp
@@ -0,0 +1,68 @@
+﻿#include <iostream>
+#include <cstdlib>
+#include <math.h>
+#include <conio.h>
+#include <string> 
+#include <algorithm>
+#include <ctime>
+
+using namespace std;
+int main()
+{
+	//==== array of strings of clients
+	string name_team[2] = { "ZKNO-TEAM", "LOL" };
+	string name[5] = { "Shamshin Pasha Sergeevich",
+		"Novikov Sergey Alekseevich", "Demin Atrem Aleksandrovich" ,
+	"Kalimeev Maksim Sergeevich", "Minevtsev Dmitriy Olegovich" };
+	int age[5] = { 17, 17, 16, 15, 17 };
+	int num[5] = { 7, 13, 10, 18, 15 };
+	double height[5] = { 176, 190, 185, 170, 187 };
+	double weight[5] = { 59,70,70,50,50 };
+
+	struct strc {
+		string name;
+		string name_team;
+		int num;
+		int age;
+		double height;
+		double weight;
+	};
+
+	strc data[5];
+	for (int i = 0; i < sizeof(data) / sizeof(data[0]); i++) {
+		data[i].num = num[i];
+		data[i].name = name[i];
+		data[i].height = height[i];
+		data[i].weight = weight[i];
+		data[i].age = age[i];
+	}
+	data[0].name_team = name_team[0];
+	data[1].name_team = name_team[1];
+	data[2].name_team = name_team[0];
+	data[3].name_team = name_team[0];
+	data[4].name_team = name_team[1];
+	double min = 1000;
+	int min_team = 0;
+	for (int i = 0; i < sizeof(name_team) / sizeof(name_team[0]); i++) {
+		double average_age = 0;
+		int counter = 0;
+		for (int j = 0; j < sizeof(data) / sizeof(data[0]); j++) {
+			if (data[j].name_team == name_team[i]) {
+				counter++;
+				average_age += data[j].age;
+			}
+		}
+		if (min > average_age / counter) {
+			min = average_age / counter;
+			min_team = i;
+		}
+	}
+	cout << min << " - " << name_team[min_team] << endl;
+	for (int i = 0; i < sizeof(data) / sizeof(data[0]); i++) {
+		if (data[i].name_team == name_team[min_team]) {
+			cout << data[i].name << " age:" << data[i].age << " h:" << data[i].height 
+				<< " w:" << data[i].weight << data[i].num << endl;
+		}
+	}
+	return 0;
+}
---
+++ ОАИП/Lab_8/Lab_8/Lab_8.cpp
@@ -0,0 +1,71 @@
+﻿#include <iostream>
+#include <cstdlib>
+#include <math.h>
+#include <conio.h>
+#include <iomanip>
+using namespace std;
+
+long double fact(int N)
+{
+	if (N < 0)
+		return 0;
+	if (N == 0)
+		return 1; 
+	else 
+		return N * fact(N - 1); 
+}
+
+double Y(double x) {
+	return pow(1+x, 1/3.);
+}
+
+double S(double x, double n) {
+	double sum = 0;
+	int koef = 2;
+	int last_number = 2;
+	for (int k = 0; k < n; k++) {
+		
+		if (k == 0) {
+			sum += 1;
+		}
+		else if (k == 1) {
+			sum += (x / 3.);
+		}
+		else if (k == 2) {
+			sum -= pow(x, 2) / 9.;
+		}
+		
+		else {
+			
+			sum += pow(-1, k - 1) * koef * (3 * k - 4) / fact(k) / pow(3, k) * pow(x, k);
+			last_number += 3;
+			koef *= last_number;
+			
+		}
+		
+	}
+	return sum;
+}
+
+int main()
+{
+	cout << "x" << setw(15) << "Y(x)" << setw(15) << "S(x)" << setw(20) << "iterations" 
+		<< endl << "--------------------------------------------------" << endl;
+
+	double epsilon = pow(10, -3);
+	
+	
+	for (double a = 0;a <= 0.8; a += 0.1) {
+		//cout << a << endl;
+		a = round(a * 100) / 100;
+		
+		for (int i = 1; ;i++) {
+			
+			if (fabs(Y(a) - S(a, i)) <= epsilon) {
+				cout << a << setw(15) << Y(a) << setw(15) << S(a, i) << setw(10) << i << endl;
+				break;
+			}
+		}
+	}
+	
+}
