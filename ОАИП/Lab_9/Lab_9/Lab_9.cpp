34221f90e12b65506f7b8de25d8800be128bc248
psshamshin <57655945+psshamshin@users.noreply.github.com>
2021-02-14 23:44:06 +0300

    Add files via upload

---
+++ ОАИП/Lab_9/Lab_9/Lab_9.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <cstdlib>
+#include <math.h>
+#include <conio.h>
+#include <algorithm>
+using namespace std;
+
+int Count(int n)
+{
+	if (n / 10 != 0) return 1 + Count(n / 10);
+	else return 1;
+}
+
+int main()
+{
+	int number = 123456;
+    cout << Count(number) << endl;
+	//cout << s << endl;
+
+	int s = 0;
+	while (number > 0) {
+		s++;
+		number = number / 10;
+	}
+	cout << s;
+}
